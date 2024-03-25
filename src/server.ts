034f379a8f5e15a545ef5ada513eebbfe18679d7
Kutub Udd <socialnetwork98710@gmail.com>
2024-03-25 23:19:31 +0600

    added

--- src/server.ts
+++ src/server.ts
@@ -7,10 +7,10 @@ let server: Server;
 
 function main() {
   try {
-    mongoose.connect(
-      'mongodb+srv://Hafejia-Travel-Tours:3zzwkF4c39lL3Mpp@cluster0.mlxcjcs.mongodb.net/Hafejia-Travel-Tours?retryWrites=true&w=majority&appName=Cluster0',
-    );
-    // mongoose.connect(config.database_url as string);
+    // mongoose.connect(
+    //   'mongodb+srv://Hafejia-Travel-Tours:3zzwkF4c39lL3Mpp@cluster0.mlxcjcs.mongodb.net/Hafejia-Travel-Tours?retryWrites=true&w=majority&appName=Cluster0',
+    // );
+    mongoose.connect(config.database_url as string);
 
     server = app.listen(config.port, () => {
       console.log(`app is listening on port ${config.port}`);
